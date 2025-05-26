4a4033409d819de2a1fdbcb81053bdd67e943d19
Vladislav Sidorenko <vladislavsidorenko2000@gmail.com>
2025-05-26 17:42:39 +0300

    feat: add SearchOrderInput component

---
+++ app/components/inputs/SearchOrderInput.tsx
@@ -0,0 +1,12 @@
+function SearchOrderInput() {
+
+  return (
+    <input
+      className="flex w-[100%] flex-row px-[24px] py-[10px] gap-[10px] border border-[#EDEDED] rounded-sm text-[#ABABAB] text-[14px]"
+      name="searchField"
+      placeholder="Search by order number"
+    />
+  );
+}
+
+export default SearchOrderInput;
