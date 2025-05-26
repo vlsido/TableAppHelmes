b9749c05646703858c1bea10ccf13df061a6e521
Vladislav Sidorenko <vladislavsidorenko2000@gmail.com>
2025-05-26 22:34:42 +0300

    perf: adjust SearchQueryInputProps interface

--- app/components/inputs/SearchQueryInput.tsx
+++ app/components/inputs/SearchQueryInput.tsx
@@ -1,15 +1,17 @@
-interface SearchQueryInput {
-  onChange: () => void;
+interface SearchQueryInputProps {
+  value: string;
+  onChange: (text: string) => void;
 }
 
-function SearchQueryInput() {
+function SearchQueryInput(props: SearchQueryInputProps) {
 
   return (
     <input
       className="flex w-[100%] flex-row px-[24px] py-[10px] gap-[10px] border border-[#EDEDED] rounded-sm text-[#ABABAB] text-[14px]"
       name="searchField"
       placeholder="Search by order number"
-
+      value={props.value}
+      onChange={(e) => props.onChange(e.target.value)}
     />
   );
 }
