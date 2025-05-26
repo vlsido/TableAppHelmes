8d322b5bfeb4e70094385dd1a247ba4c9e4c9947
Vladislav Sidorenko <vladislavsidorenko2000@gmail.com>
2025-05-26 16:30:55 +0300

    refactor: remove template content

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,12 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Table App Helmes" },
+    { name: "description", content: "Welcome to the Helmes's Assignment!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <></>;
 }
