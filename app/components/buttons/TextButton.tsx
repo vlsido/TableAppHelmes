6719aa823100c9345398d76abe0233d33ee8ae0e
Vladislav Sidorenko <vladislavsidorenko2000@gmail.com>
2025-05-28 01:23:49 +0300

    feat: add 'disabled' prop to TextButton

--- app/components/buttons/TextButton.tsx
+++ app/components/buttons/TextButton.tsx
@@ -1,13 +1,14 @@
 import type { ReactElement } from "react";
 
 interface TextButtonProps {
+  testId: string;
   text: string;
   onPress: () => void;
   ariaLabel: string;
-  testId: string;
   leftSideIcon?: ReactElement;
   backgroundColor?: string;
   color?: string;
+  disabled?: boolean;
 }
 
 function TextButton(props: TextButtonProps) {
@@ -17,6 +18,7 @@ function TextButton(props: TextButtonProps) {
       className="flex flex-row items-center px-[16px] py-[10px] gap-[8px] text-black bg-red rounded-sm"
       data-testid={props.testId}
       aria-label={props.ariaLabel}
+      disabled={props.disabled}
       style={{
         backgroundColor: props.backgroundColor ? props.backgroundColor : "#005EFF",
         color: props.color ? props.color : "#fff"
