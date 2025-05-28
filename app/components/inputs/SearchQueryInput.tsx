d9dffe04550d08a396022cf04394c24d1503b0a1
Vladislav Sidorenko <vladislavsidorenko2000@gmail.com>
2025-05-28 14:28:18 +0300

    refactor: change input field aria-label

--- app/components/inputs/SearchQueryInput.tsx
+++ app/components/inputs/SearchQueryInput.tsx
@@ -8,7 +8,8 @@ function SearchQueryInput(props: SearchQueryInputProps) {
   return (
     <input
       data-testid="SEARCH_QUERY_INPUT.CONTAINER:INPUT"
-      aria-label="Search orders"
+      aria-label="Order number"
+      role="search"
       className="flex w-[100%] flex-row px-[24px] py-[10px] gap-[10px] border border-[#EDEDED] rounded-sm text-[#ABABAB] text-[14px]"
       name="searchField"
       placeholder="Search by order number"
